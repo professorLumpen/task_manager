6f9761b653d471e54df0079938b1a2d3e54deb19
professorLumpen <jeanpaulvsratyi@gmail.com>
2025-08-09 22:11:30 +0300

    Refactored router name and included routers in app

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 
+from app.api.endpoints.tasks import tasks_router
+from app.api.endpoints.users import users_router
 from app.core.config import settings
 
 
 app = FastAPI()
+app.include_router(users_router)
+app.include_router(tasks_router)
 
 if settings.DEBUG:
     app.debug = True
