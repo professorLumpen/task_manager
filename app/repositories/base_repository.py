1c191a91638faf23d7c39df3c03e0ddb19805018
professorLumpen <jeanpaulvsratyi@gmail.com>
2025-08-10 20:59:36 +0300

    Added unit of work

--- app/repositories/base_repository.py
+++ app/repositories/base_repository.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 from fastapi import HTTPException
-from sqlalchemy import delete, insert, select
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -34,9 +34,9 @@ class Repository(AbstractRepository):
         self.session = session
 
     async def add_one(self, data: dict):
-        query = insert(self.model).values(**data).returning(self.model)
-        result = await self.session.execute(query)
-        return result.scalar_one()
+        new_obj = self.model(**data)
+        self.session.add(new_obj)
+        return new_obj
 
     async def find_all(self):
         query = select(self.model)
@@ -53,11 +53,9 @@ class Repository(AbstractRepository):
         obj = await self.find_one(obj_id)
         for key, val in new_data.items():
             setattr(obj, key, val)
-        await self.session.commit()
         return obj
 
     async def remove_one(self, obj_id: int):
-        query = delete(self.model).where(self.model.id == obj_id)
-        await self.session.execute(query)
-        await self.session.commit()
-        return obj_id
+        obj = await self.find_one(obj_id)
+        await self.session.delete(obj)
+        return obj
---
+++ app/utils/unit_of_work.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+
+from app.repositories.task_repository import TaskRepository
+from app.repositories.user_repository import UserRepository
+
+
+class IUnitOfWork(ABC):
+    task_repo: TaskRepository
+    user_repo: UserRepository
+
+    @abstractmethod
+    async def __aenter__(self):
+        pass
+
+    @abstractmethod
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    @abstractmethod
+    async def commit(self):
+        pass
+
+    @abstractmethod
+    async def rollback(self):
+        pass
+
+
+class UnitOfWork(IUnitOfWork):
+    def __init__(self, session_maker):
+        self.session_maker = session_maker
+
+    async def __aenter__(self):
+        self.session = await self.session_maker()
+        self.user_repo = UserRepository(self.session)
+        self.task_repo = TaskRepository(self.session)
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.rollback()
+        await self.session.close()
+        self.session = None
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
